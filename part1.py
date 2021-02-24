f7bbed082a35472ef549a18d3f9f808523d9ee57
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-02-24 16:50:14 +0100

    update function + 1 test

--- part1.py
+++ part1.py
@@ -14,18 +14,16 @@ def facto_cholesky(A):
     for i in range(0, columns):
         for j in range(i, lines):
 
-            sum = 0
-            value = 0
-
                 # Cas diagonale
             if (i == j):
-                
-                for k in range(0, i-1):
+                sum = 0
+                for k in range(0, i):
                     sum += L[i][k]**2
                 value = sqrt(A[j][i] - sum)
 
             else:
-                for k in range(0, i-1):
+                sum = 0
+                for k in range(0, i):
                     sum += L[j][k] * L[i][k]
                 value = (A[j][i] - sum) / L[i][i]
                 
@@ -33,5 +31,26 @@ def facto_cholesky(A):
 
     return L
 
-L = facto_cholesky(a)
-print(L)
+def test_facto_cholesky():
+
+    #Test 1
+    A = np.array( [ [4, -2, -4], [-2, 10, 5], [-4, 5, 6] ])
+    L = facto_cholesky(A)
+    Lt = L.T
+    test_A = np.dot(L, Lt)
+
+    if ( (A == test_A).all()):
+        return True
+    return False
+    
+
+#L = facto_cholesky(a)
+#print(test_facto_cholesky())
+
+"""
+1) Complexité n^3
+"""
+
+def symetrik_generator():
+    print("test")
+
