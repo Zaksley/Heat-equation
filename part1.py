17f33eb07f8dc677408db9773028196ad2a829b3
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-03-03 15:55:37 +0100

    part 1 update

--- part1.py
+++ part1.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import *
+from random import randint
 
 a = np.array( [ [4, -2, -4], [-2, 10, 5], [-4, 5, 6] ])
 
@@ -51,6 +52,50 @@ def test_facto_cholesky():
 1) Complexité n^3
 """
 
-def symetrik_generator():
-    print("test")
+ # A = B (généré aléat) + Bt ==> A symétrique
+def symetrik_generator(N):
+    B = np.zeros(shape=(3, 3))
+    Bt = B.T
+    lines = B.shape[0]
+    columns = B.shape[1]
+
+        # Erreur dans la value N
+    if (N < 0): return 0
+
+    nb_zero = (lines * (columns-1)/2) - N
+
+        # Rempli la matrice avec valeurs aléatoires sauf cas particulier éléments diagonaux
+    for i in range(0, columns):
+        sum = 0
+        for j in range(0, lines):
+                # On gère les éléments diagonaux à la fin
+            if (i == j):
+                continue
+
+            value = randint(-100, 100)
+            sum += abs(value)
+            B[i][j] = value
+
+        B[i][i] = randint(sum+1, sum+100)
+
+    A = B + Bt
+
+        # Annule un maximum d'éléments non diagonaux en fonction du paramètre N
+    for i in range(0, columns):
+        for j in range(0, lines):
+            if (i == j):
+                continue
+
+            if (nb_zero > 0):
+                A[i][j] = 0
+                A[j][i] = 0
+                nb_zero -= 1
+            else:
+                break
+
+    print(A)
+    return A
+
+symetrik_generator()
+
 
