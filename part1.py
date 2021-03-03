151bf5dca1978995f8e876ba1eb381b956337359
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-03-03 18:22:16 +0100

    Finish part 1

--- part1.py
+++ part1.py
@@ -7,6 +7,8 @@ from random import randint
 #a = np.array( [ [4, -2, -4], [-2, 10, 5], [-4, 5, 6] ])
 #print(a)
 
+# (Question 1)
+
 def facto_cholesky(A):
         #Initialize L
     L = np.zeros(shape=A.shape)
@@ -53,9 +55,11 @@ def test_facto_cholesky():
 1) Complexité n^3
 """
 
+# (Question 3)
+
  # A = B (généré aléat) + Bt ==> A symétrique
-def symetrik_generator(N):
-    B = np.zeros(shape=(4, 4))
+def symetrik_generator(N, size):
+    B = np.zeros(shape=(size, size))
     Bt = B.T
     lines = B.shape[0]
     columns = B.shape[1]
@@ -102,6 +106,8 @@ def test_symetrik_generator():
         print(symetrik_generator(3))
 """
 
+# (Question 4)
+
 def facto_cholesky_incomplete(A):
         #Initialize L
     L = np.zeros(shape=A.shape)
@@ -145,7 +151,7 @@ def test_facto_cholesky_incomplete():
 
             # N dépends de la taille des matrices
         # => Attention donc à la shape fourni par symetrik_generator()
-        A = symetrik_generator(randint(0, 3))
+        A = symetrik_generator(randint(0, 3), 4)
         L = facto_cholesky_incomplete(A)
         Lt = L.T
         test_A = np.dot(L, Lt)
@@ -167,7 +173,7 @@ def test_time_cholesky():
     
     # Batterie de test sur 100 matrices générés - CHOLESKY NORMAL
     for z in range(nb_tests):
-        A = symetrik_generator(0)
+        A = symetrik_generator(0, 4)
         L = facto_cholesky(A)
 
     tps2 = time.time()
@@ -176,7 +182,7 @@ def test_time_cholesky():
     tps1 = time.time()
     # Batterie de test sur 100 matrices générés - CHOLESKY INCOMPLETE
     for z in range(nb_tests):
-        A = symetrik_generator(4)
+        A = symetrik_generator(4, 4)
         L = facto_cholesky_incomplete(A)
 
     tps2 = time.time()
@@ -185,7 +191,77 @@ def test_time_cholesky():
     print(time_cholesky)
     print(time_cholesky_incomplete)
 
+#test_time_cholesky()
 
-test_time_cholesky()
+"""
+    Résultats peu concluants:
+        => Les différences sont très très faibles car on diminue seulement d'un petit nombre les calculs
+"""
 
 
+    # Ici on essaye sur des grandes matrices
+# => Matrice (100, 100) par exemple
+# => On modifie symetrik_generator pour cela
+def test_cholesky_big_matrices():
+    
+    
+    # - CHOLESKY NORMAL
+
+    A = symetrik_generator(0, 700)
+    tps1 = time.time()
+    L = facto_cholesky(A)
+    tps2 = time.time()
+    time_cholesky = (tps2 - tps1)
+
+    
+    # - CHOLESKY INCOMPLETE
+    A = symetrik_generator(700*699/2, 700)
+    tps1 = time.time()
+    L = facto_cholesky_incomplete(A)
+    tps2 = time.time()
+    time_cholesky_incomplete = (tps2 - tps1)
+
+    print(time_cholesky)
+    print(time_cholesky_incomplete)
+
+#test_cholesky_big_matrices()
+
+"""
+Résultats:
+    Sur une matrice (700, 700) avec UNIQUEMENT des éléments extra diagonaux = 0
+        Temps d'execution de facto_cholesky : 25s
+        Temps d'execution de facto_cholesky_imcomplet : 0.20s 
+
+    Sur une matrice (700, 700) avec UNIQUEMENT des éléments extra diagonaux != 0
+    Temps d'execution de facto_cholesky : 26s
+    Temps d'execution de facto_cholesky_imcomplet : 27s 
+"""
+
+# (Question 5)
+
+def isGoodConditionneur(A):
+    T = facto_cholesky(A)
+    Tt = T.T
+
+    invert_T = np.linalg.inv(T)
+    invert_Tt = np.linalg.inv(Tt)
+
+    return np.linalg.cond( np.dot(invert_Tt, invert_T, A) )
+
+
+def test_conditionneur():
+
+    A = symetrik_generator(3, 3)
+    cond_A = np.linalg.cond(A)
+
+    print("Valeur de notre calcul : " + str(isGoodConditionneur(A)))
+    print("Valeur de cond(A) : " + str(cond_A))
+
+
+test_conditionneur()
+
+"""
+    Résultats :
+        Si notre résultat > cond(A) => Mauvais préconditionneur
+        Sinon => Bon conditionneur
+"""
